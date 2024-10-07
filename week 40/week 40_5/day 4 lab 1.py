94486ad011c830c9bba2538a1fe8318bffe436ee
Derek Homer <111864360+Dalhom@users.noreply.github.com>
2024-10-07 13:43:38 +0200

    Update day 4 lab 1.py

---
+++ week 40/week 40_5/day 4 lab 1.py
@@ -0,0 +1,17 @@
+"""
+
+1.
+Understand what a lambda function is and when to use it in Python.
+Task:
+Research and explain what a lambda  function is.
+Describe situations where using a lambda  function would be more appropriate than defining a full function using def.
+Explain the following terms in relation to lambda: Anonymous function, Inline function
+
+
+
+- A lambda function in Python is a small, anonymous function defined using the lambda keyword. Unlike regular named functions defined with def, a lambda function is typically used for short, simple operations and can only contain a single expression (no multi-line statements).
+- Lambda functions are more appropriate for for small tasks that are one-time use and can be written in one line, for example in list manipulations, sorting, or filtering operations. It also improves readability since it keeps the focus on the main operation rather than a separate function defined elsewhere in the code.
+- It's referred to as an anonymous because it doesn't require a name 
+- It's referred to as inline function because it is typically used directly where it's needed. The lambda expression is written in the same place where the function call occurs
+
+"""
